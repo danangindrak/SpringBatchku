0dc4e70ec8b7bf673cc8f08dad3b2a0eb73177b2
danangindrak <danangindrak@gmail.com>
2016-07-21 00:51:12 +0700

    update project example

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-SpringBatchku
-=============
+# SpringBatchku
 
-Tools and libraries used :
-Maven 3
-Eclipse 4.2
-JDK 1.6
-Spring Core 3.2.2.RELEASE
-Spring OXM 3.2.2.RELEASE
-Spring Batch 2.2.0.RELEASE
-MySQL Java Driver 5.1.25
+This application is a batch downloading application data from the database with sample user table into a csv file.
+
+## Tools and libraries :
+* Maven 3
+* Eclipse 4.2
+* JDK 1.6
+* Spring Core 3.2.2.RELEASE
+* Spring OXM 3.2.2.RELEASE
+* Spring Batch 2.2.0.RELEASE
+* MySQL Java Driver 5.1.25
---
+++ schemaexample/datalengkap.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.6.15 : Database - testbatch
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`testbatch` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `testbatch`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_login` varchar(12) DEFAULT NULL,
+  `user_pass` varchar(11) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`user_login`,`user_pass`,`age`) values (1,'danang','danange236',23),(2,'indra','hjfbakdjf',33),(3,'kurniawan','sdfsdf',32),(4,'cakep','adfkfj',31),(5,'sekali','akjf',22),(6,'danange','sdfjlsdf',11);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
